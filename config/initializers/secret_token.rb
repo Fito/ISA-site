9dbbd126ee18a5882bc78e25c4832a707c1598c8
Fito <fito@paragil.com>
2013-03-09 10:42:11 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IsaSite::Application.config.secret_token = '2ca7c18c3bf4e55119e64d60b0834cb9b63cb290b17c79997131f47dce5f8a18f439083c0a1142f5cf780ef9be1c7976bad8c0b11af86193ab451e8a08b01229'
