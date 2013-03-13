2bef168e380c5e50b502838578fce1053bb70b29
Fito <fito@paragil.com>
2013-03-13 08:58:39 -0700

    name attribute added to Admin model

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -5,6 +5,6 @@ class Admin < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :name
   has_many :posts
 end
--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe Admin do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @admin = Admin.new(:email => 'my@email.com', :password => 'passpass', :name => 'my name')
+  end
+  
+  it 'Should have a name' do
+    @admin.name.should_not be_nil
+  end
+  
+  it 'Should have the right name' do
+    @admin.name.should == 'my name'
+  end
 end
